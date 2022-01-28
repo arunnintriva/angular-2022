cf89e21d2153031c18892608dc4abc54dbaab3f3
arun raju <arun@nintrivalabs.com>
2022-01-28 12:47:31 +0530

    Add angular material

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -1,12 +1,13 @@
 import { BrowserModule } from '@angular/platform-browser';
 import { NgModule } from '@angular/core';
-import { FormsModule } from '@angular/forms';
+import { BrowserAnimationsModule } from '@angular/platform-browser/animations';
 
 import { AppComponent } from './app.component';
+import { Materialmodule } from './material.module';
 
 @NgModule({
   declarations: [AppComponent],
-  imports: [BrowserModule, FormsModule],
+  imports: [BrowserModule, BrowserAnimationsModule, Materialmodule],
   providers: [],
   bootstrap: [AppComponent],
 })
